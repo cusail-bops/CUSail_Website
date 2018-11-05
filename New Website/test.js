8644d8737a6b02e7f9d7d44dc86b6bd283720272
Mary Essex <mae87@cornell.edu>
2018-11-05 18:18:56 -0500

    changes

--- New Website/test.js
+++ New Website/test.js
@@ -1,98 +1,63 @@
-(function ($) {
-    $.fn.countTo = function (options) {
-        options = options || {};
-        
-        return $(this).each(function () {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-                from:            $(this).data('from'),
-                to:              $(this).data('to'),
-                speed:           $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals:        $(this).data('decimals')
-            }, options);
-            
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
-            
-            // references & variables that will change with each update
-            var self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
-            
-            $self.data('countTo', data);
-            
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
-            }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-            
-            // initialize the element with the starting value
-            render(value);
-            
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-                
-                render(value);
-                
-                if (typeof(settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-                
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-                    
-                    if (typeof(settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-            
-            function render(value) {
-                var formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue);
-            }
-        });
-    };
-    
-    $.fn.countTo.defaults = {
-        from: 0,               // the number the element should start at
-        to: 0,                 // the number the element should end at
-        speed: 100,           // how long it should take to count between the target numbers
-        refreshInterval: 10,  // how often the element should be updated
-        decimals: 0,           // the number of decimal places to show
-        formatter: formatter,  // handler for formatting the value before rendering
-        onUpdate: null,        // callback method for every time the element is updated
-        onComplete: null       // callback method for when the element finishes updating
-    };
-    
-    function formatter(value, settings) {
-        return value.toFixed(settings.decimals);
-    }
-}(jQuery));
+////////////////////////////////////
+// NAVIGATION SHOW/HIDE
 
-jQuery(function ($) {
-  // custom formatting example
-  $('.count-number').data('countToOptions', {
-    formatter: function (value, options) {
-      return value.toFixed(options.decimals);
+$("nav ul").hide();
+
+$(".nav-toggle").click( function() {
+  $("nav ul").slideToggle("medium");
+});
+
+$("nav ul li a, .brand a").click( function() {
+  $("nav ul").hide();
+});
+
+////////////////////////////////////
+// SMOOTH SCROLLING WITH NAV HEIGHT OFFSET
+
+$(function() {
+  var navHeight = $("nav").outerHeight();
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - navHeight
+        }, 1000);
+        return false;
+      }
     }
   });
-  
-  // start all the timers
-  $('.timer').each(count);  
-  
-  function count(options) {
-    var $this = $(this);
-    options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-    $this.countTo(options);
+});
+
+////////////////////////////////////
+// NAVIGATION STICKY
+
+var viewHeight = $(window).height();
+var navigation = $('nav');
+
+$(window).scroll( function() {
+  if ( $(window).scrollTop() > (viewHeight - 175) ) { //edit for nav height
+    navigation.addClass('sticky');
+  } else {
+    navigation.removeClass('sticky');
   }
+});
+
+////////////////////////////////////////////////
+// MAKE THE SPLASH CONTAINER VERTICALLY CENTERED
+
+function centerSplash() {
+  var navHeight = $("nav").outerHeight();
+  var splashHeight = $(".splash .container").height();
+  var remainingHeight = $(window).height() - splashHeight - navHeight;
+  $(".splash .container").css({"padding-top" : remainingHeight/2, "padding-bottom" : remainingHeight/2});
+}
+
+$( document ).ready( function() {
+  centerSplash();
+});
+
+$( window ).resize( function() {
+  centerSplash();
 });
