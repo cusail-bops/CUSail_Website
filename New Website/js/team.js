d347aa560ff7b257376960dcb864b593dfa18629
Tomas Engquist <te68@cornell.edu>
2019-11-10 17:14:40 -0500

    added unerline to navbar

--- New Website/js/team.js
+++ New Website/js/team.js
@@ -1,18 +1,9 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const navbar = document.getElementById("subteam-navbar")
 
-// Get the navbar
-var navbar = document.getElementsByClassName("desktopsubteams")[0]
-console.log(navbar)
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+navbar.addEventListener('click', function(){
+  currentUnderline = document.getElementsByClassName("underline")
+  if (currentUnderline[0]){
+    currentUnderline[0].classList.remove("underline")
   }
-}
+  event.target.classList.add("underline")
+})
