81f00576b97a923eaaf3a6f3ddd3a4c5fe1259ca
Tomas Engquist <te68@cornell.edu>
2019-11-17 16:42:22 -0500

    fixed mobile subteam navbar

--- New Website/js/team.js
+++ New Website/js/team.js
@@ -1,6 +1,20 @@
-const navbar = document.getElementById("subteam-navbar")
+console.log(document.body.clientWidth)
+
+let selector
+
+if (window.screen.width <= 767) {
+  selector = "subteam-navbar-mobile"
+}
+else{
+  selector = "subteam-navbar"
+}
+
+console.log(selector)
+
+const navbar = document.getElementById(selector)
 
 navbar.addEventListener('click', function(){
+  console.log("clicked")
   currentUnderline = document.getElementsByClassName("underline")
   if (currentUnderline[0]){
     currentUnderline[0].classList.remove("underline")
