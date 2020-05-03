c4316bf3ea86cf23216eb44fd59b1f0b397d9a48
Srikar Yallala <srikaryallala@gmail.com>
2020-05-03 17:20:13 -0500

    Added content

---
+++ New Website 2.0/js/fleet.js
@@ -0,0 +1,7 @@
+function changeImage(a) {
+    document.getElementById("boatpicmain").src=a;
+}
+
+function reset() {
+    document.getElementById("boatpicmain").src="..\\images\\interactivefleet\\boat_diagram_outline.png";
+}
