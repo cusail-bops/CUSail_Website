587dd609edfeb128fb03f289615427aef2ba9f9a
Tomas Engquist <te68@cornell.edu>
2020-05-05 17:29:51 -0400

    index animations

---
+++ New Website 2.0/js/index.js
@@ -0,0 +1,139 @@
+const metrics = Array.from(document.getElementsByClassName('metric'))
+const pictures = Array.from(document.getElementsByClassName('pic'))
+const eventCards = Array.from(document.getElementsByClassName('event-card'))
+const goals = Array.from(document.getElementsByClassName('goal'))
+
+const width = window.innerWidth
+const addHidden = (array) => {
+  array.forEach((item) => {
+    item.classList.add('hidden')
+  });
+}
+const displayItems = (items, start, end, animation) => {
+  for (i = start;i < end; i ++){
+    items[i].classList.add(animation)
+    items[i].classList.remove('hidden')
+  }
+}
+
+if (width >= 623){
+  addHidden(metrics)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 550 && y < 1050){
+      displayItems(metrics, 0,1, 'slide-right')
+      displayItems(metrics, 1,2, 'zoom-in')
+      displayItems(metrics, 2,3, 'slide-left')
+    }
+})
+}
+
+if (width >= 1048){
+  addHidden(pictures)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 800 && y < 1400){
+      displayItems(pictures, 0,2, 'slide-right')
+      displayItems(pictures, 2,4, 'slide-left')
+    }
+})
+}
+
+if (width >= 785 && width <= 1048){
+  addHidden(pictures)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 900 && y < 1500){
+      displayItems(pictures, 0,1, 'slide-right')
+      displayItems(pictures, 1,2, 'zoom-in')
+      displayItems(pictures, 2,3, 'slide-left')
+    }
+    if (y > 1200 && y < 1800){
+      displayItems(pictures, 3,4, 'zoom-in')
+    }
+})
+}
+
+if (width >= 526 && width <= 785){
+  addHidden(pictures)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    console.log(y)
+    if (y > 900 && y < 1500){
+      displayItems(pictures, 0,1, 'slide-right')
+      displayItems(pictures, 1,2, 'slide-left')
+    }
+    if (y > 1200 && y < 1800){
+      displayItems(pictures, 2,3, 'slide-right')
+      displayItems(pictures, 3,4, 'slide-left')
+    }
+})
+}
+
+if (width >= 1044 && width <= 1391){
+  addHidden(eventCards)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 1600 && y < 2100){
+      displayItems(eventCards, 0,1, 'slide-right')
+      displayItems(eventCards, 1,2, 'zoom-in')
+      displayItems(eventCards, 2,3, 'slide-left')
+    }
+    if (y > 2100 && y < 2600){
+      displayItems(eventCards, 3,4, 'slide-right')
+      displayItems(eventCards, 4,5, 'slide-left')
+    }
+})
+}
+
+if (width >= 696 && width <= 1043){
+  addHidden(eventCards)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 1900 && y < 2400){
+      displayItems(eventCards, 0, 1, 'slide-right')
+      displayItems(eventCards, 1, 2, 'slide-left')
+    }
+    if (y > 2400 && y < 2900){
+      displayItems(eventCards, 2,3, 'slide-right')
+      displayItems(eventCards, 3,4, 'slide-left')
+    }
+    if (y > 2900 && y < 3400){
+      displayItems(eventCards, 4,5, 'zoom-in')
+
+    }
+})
+}
+
+if (width >= 700  && width < 900){
+  addHidden(goals)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    if (y > 3600 && y < 4000){
+      displayItems(goals, 0, 1, 'slide-left')
+    }
+    if (y > 3900 && y < 4300){
+      displayItems(goals, 1, 2, 'slide-left')
+    }
+    if (y > 4200 && y < 4600){
+      displayItems(goals, 2, 3, 'slide-left')
+    }
+})
+}
+
+if (width >= 900  && width <= 1391){
+  addHidden(goals)
+  window.addEventListener('scroll', (event) => {
+    let y = window.scrollY
+    console.log(y)
+    if (y > 2600 && y < 3000){
+      displayItems(goals, 0, 1, 'slide-left')
+    }
+    if (y > 2800 && y < 3300){
+      displayItems(goals, 1, 2, 'slide-left')
+    }
+    if (y > 3100 && y < 3600){
+      displayItems(goals, 2, 3, 'slide-left')
+    }
+})
+}
--- New Website 2.0/js/ourteam.js
+++ New Website 2.0/js/ourteam.js
@@ -1,289 +1,75 @@
 const flipCards = Array.from(document.getElementsByClassName('member-card'))
-if (window.innerWidth >= 1333  && window.innerWidth <= 1600){
+const width = window.innerWidth
+const addHidden = () => {
   flipCards.forEach((card, i) => {
     card.classList.add('hidden')
   });
-
+}
+const displayCards = (start, end) => {
+  for (i = start;i < end; i ++){
+    flipCards[i].classList.add('zoom-in')
+    flipCards[i].classList.remove('hidden')
+  }
+}
+if (width >= 1333  && width <= 1600){
+  addHidden()
   window.addEventListener('scroll', (event) => {
     let y = window.scrollY
-    if (y > 700 && y < 1300){
-        for (i = 0;i < 4; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 1400 && y < 2000){
-        for (i = 4;i < 8; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 1800 && y < 2400){
-        for (i = 8;i < 12; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2200 && y < 2800){
-        for (i = 12;i < 16; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-
-    if (y > 2600 && y < 3200){
-        for (i = 16;i < 17; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-
-    if (y > 3400 && y < 4000){
-        for (i = 17;i < 21; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 3800 && y < 4400){
-        for (i = 21;i < 25; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 4200 && y < 4800){
-        for (i = 25;i < 27; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5000 && y < 5600){
-        for (i = 27;i < 31; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5400 && y < 6000){
-        for (i = 31;i < 34; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
+    if (y > 700 && y < 1300){displayCards(0, 4)}
+    if (y > 1400 && y < 2000){displayCards(4, 8)}
+    if (y > 1800 && y < 2400){displayCards(8, 12)}
+    if (y > 2200 && y < 2800){displayCards(12, 16)}
+    if (y > 2600 && y < 3200){displayCards(16, 17)}
+    if (y > 3400 && y < 4000){displayCards(17, 21)}
+    if (y > 3800 && y < 4400){displayCards(21, 25)}
+    if (y > 4200 && y < 4800){displayCards(25, 27)}
+    if (y > 5000 && y < 5600){displayCards(27, 31)}
+    if (y > 5400 && y < 6000){displayCards(31, 34)}
   })
-
 }
 
-if (window.innerWidth <= 1332  && window.innerWidth >= 1015){
-  flipCards.forEach((card, i) => {
-    card.classList.add('hidden')
-  });
-
+if (width <= 1332  && width >= 1015){
+  addHidden()
   window.addEventListener('scroll', (event) => {
     let y = window.scrollY
-    if (y > 500 && y < 1100){
-        for (i = 0;i < 3; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 900 && y < 1500){
-        for (i = 3;i < 4; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 1700 && y < 2300){
-        for (i = 4;i < 7; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2100 && y < 2700){
-        for (i = 7;i < 10; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2500 && y < 3100){
-        for (i = 10;i < 13; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2900 && y < 3500){
-        for (i = 13;i < 16; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 3300 && y < 3900){
-        for (i = 16;i < 17; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 4200 && y < 4800){
-        for (i = 17;i < 20; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 4600 && y < 5200){
-        for (i = 20;i < 23; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5000 && y < 5600){
-        for (i = 23;i < 26; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5400 && y < 6000){
-        for (i = 26;i < 27; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 6200 && y < 6800){
-        for (i = 27;i < 30; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 6600 && y < 7200){
-        for (i = 30;i < 33; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 7000 && y < 7600){
-        for (i = 33;i < 34; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-
+    if (y > 500 && y < 1100){displayCards(0, 3)}
+    if (y > 900 && y < 1500){displayCards(3, 4)}
+    if (y > 1700 && y < 2300){displayCards(4, 7)}
+    if (y > 2100 && y < 2700){displayCards(7, 10)}
+    if (y > 2500 && y < 3100){displayCards(10, 13)}
+    if (y > 2900 && y < 3500){displayCards(13, 16)}
+    if (y > 3300 && y < 3900){displayCards(16, 17)}
+    if (y > 4200 && y < 4800){displayCards(17, 20)}
+    if (y > 4600 && y < 5200){displayCards(20, 23)}
+    if (y > 5000 && y < 5600){displayCards(23, 26)}
+    if (y > 5400 && y < 6000){displayCards(26, 27)}
+    if (y > 6200 && y < 6800){displayCards(27, 30)}
+    if (y > 6600 && y < 7200){displayCards(30, 33)}
+    if (y > 7000 && y < 7600){displayCards(33, 34)}
   })
-
 }
 
-if (window.innerWidth <= 1014  && window.innerWidth >= 698){
-  flipCards.forEach((card, i) => {
-    card.classList.add('hidden')
-  });
-
+if (width <= 1014  && width >= 698){
+  addHidden()
   window.addEventListener('scroll', (event) => {
     let y = window.scrollY
-    console.log(y)
-    if (y > 500 && y < 1200){
-        for (i = 0;i < 2; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 900 && y < 1600){
-        for (i = 2;i < 4; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 1700 && y < 2400){
-        for (i = 4;i < 6; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2100 && y < 2800){
-        for (i = 6;i < 8; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2500 && y < 3200){
-        for (i = 8;i < 10; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 2900 && y < 3600){
-        for (i = 10;i < 12; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 3300 && y < 4000){
-        for (i = 12;i < 14; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 3700 && y < 4400){
-        for (i = 14;i < 16; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 4100 && y < 4800){
-        for (i = 16;i < 17; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5100 && y < 5800){
-        for (i = 17;i < 19; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5500 && y < 6200){
-        for (i = 19;i < 21; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 5900 && y < 6600){
-        for (i = 21;i < 23; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 6300 && y < 7000){
-        for (i = 23;i < 25; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 6700 && y < 7400){
-        for (i = 25;i < 27; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 7700 && y < 8400){
-        for (i = 27;i < 29; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 8100 && y < 8800){
-        for (i = 29;i < 31; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 8500 && y < 9200){
-        for (i = 31;i < 33; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
-    if (y > 8900 && y < 9600){
-        for (i = 33;i < 34; i ++){
-          flipCards[i].classList.add('zoom-in')
-          flipCards[i].classList.remove('hidden')
-        }
-    }
+    if (y > 500 && y < 1200){displayCards(0, 2)}
+    if (y > 900 && y < 1600){displayCards(2, 4)}
+    if (y > 1700 && y < 2400){displayCards(4, 6)}
+    if (y > 2100 && y < 2800){displayCards(6, 8)}
+    if (y > 2500 && y < 3200){displayCards(8, 10)}
+    if (y > 2900 && y < 3600){displayCards(10, 12)}
+    if (y > 3300 && y < 4000){displayCards(12, 14)}
+    if (y > 3700 && y < 4400){displayCards(14, 16)}
+    if (y > 4100 && y < 4800){displayCards(16, 17)}
+    if (y > 5100 && y < 5800){displayCards(17, 19)}
+    if (y > 5500 && y < 6200){displayCards(19, 21)}
+    if (y > 5900 && y < 6600){displayCards(21, 23)}
+    if (y > 6300 && y < 7000){displayCards(23, 25)}
+    if (y > 6700 && y < 7400){displayCards(25, 27)}
+    if (y > 7700 && y < 8400){displayCards(27, 29)}
+    if (y > 8100 && y < 8800){displayCards(29, 31)}
+    if (y > 8500 && y < 9200){displayCards(31, 33)}
+    if (y > 8900 && y < 9600){displayCards(33, 34)}
   })
-
 }
