94fa22af3e843c7b6835c90c52278563295832b5
Tomas Engquist <te68@cornell.edu>
2020-09-12 15:05:59 -0400

    member changes

--- js/ourteam.js
+++ js/ourteam.js
@@ -15,16 +15,15 @@ if (width >= 1333  && width <= 1600){
   addHidden()
   window.addEventListener('scroll', (event) => {
     let y = window.scrollY
-    if (y > 700 && y < 1300){displayCards(0, 4)}
-    if (y > 1400 && y < 2000){displayCards(4, 8)}
-    if (y > 1800 && y < 2400){displayCards(8, 12)}
-    if (y > 2200 && y < 2800){displayCards(12, 16)}
-    if (y > 2600 && y < 3200){displayCards(16, 17)}
-    if (y > 3400 && y < 4000){displayCards(17, 21)}
-    if (y > 3800 && y < 4400){displayCards(21, 25)}
-    if (y > 4200 && y < 4800){displayCards(25, 27)}
-    if (y > 5000 && y < 5600){displayCards(27, 31)}
-    if (y > 5400 && y < 6000){displayCards(31, 34)}
+    if (y > 400 && y < 1000){displayCards(0, 4)}
+    if (y > 1000 && y < 1600){displayCards(4, 8)}
+    if (y > 1400 && y < 2000){displayCards(8, 12)}
+    if (y > 1800 && y < 2400){displayCards(12, 16)}
+    if (y > 2200 && y < 2800){displayCards(16, 17)}
+    if (y > 3200 && y < 3800){displayCards(17, 21)}
+    if (y > 3600 && y < 4200){displayCards(21, 23)}
+    if (y > 4400 && y < 5000){displayCards(23, 27)}
+    if (y > 4800 && y < 5400){displayCards(27, 29)}
   })
 }
 
@@ -34,18 +33,15 @@ if (width <= 1332  && width >= 1015){
     let y = window.scrollY
     if (y > 500 && y < 1100){displayCards(0, 3)}
     if (y > 900 && y < 1500){displayCards(3, 4)}
-    if (y > 1700 && y < 2300){displayCards(4, 7)}
+    if (y > 1600 && y < 2200){displayCards(4, 7)}
     if (y > 2100 && y < 2700){displayCards(7, 10)}
     if (y > 2500 && y < 3100){displayCards(10, 13)}
     if (y > 2900 && y < 3500){displayCards(13, 16)}
     if (y > 3300 && y < 3900){displayCards(16, 17)}
-    if (y > 4200 && y < 4800){displayCards(17, 20)}
+    if (y > 4100 && y < 4700){displayCards(17, 20)}
     if (y > 4600 && y < 5200){displayCards(20, 23)}
     if (y > 5000 && y < 5600){displayCards(23, 26)}
-    if (y > 5400 && y < 6000){displayCards(26, 27)}
-    if (y > 6200 && y < 6800){displayCards(27, 30)}
-    if (y > 6600 && y < 7200){displayCards(30, 33)}
-    if (y > 7000 && y < 7600){displayCards(33, 34)}
+    if (y > 5400 && y < 6000){displayCards(26, 29)}
   })
 }
 
@@ -55,21 +51,18 @@ if (width <= 1014  && width >= 698){
     let y = window.scrollY
     if (y > 500 && y < 1200){displayCards(0, 2)}
     if (y > 900 && y < 1600){displayCards(2, 4)}
-    if (y > 1700 && y < 2400){displayCards(4, 6)}
+    if (y > 1600 && y < 2300){displayCards(4, 6)}
     if (y > 2100 && y < 2800){displayCards(6, 8)}
     if (y > 2500 && y < 3200){displayCards(8, 10)}
     if (y > 2900 && y < 3600){displayCards(10, 12)}
     if (y > 3300 && y < 4000){displayCards(12, 14)}
     if (y > 3700 && y < 4400){displayCards(14, 16)}
     if (y > 4100 && y < 4800){displayCards(16, 17)}
-    if (y > 5100 && y < 5800){displayCards(17, 19)}
-    if (y > 5500 && y < 6200){displayCards(19, 21)}
-    if (y > 5900 && y < 6600){displayCards(21, 23)}
-    if (y > 6300 && y < 7000){displayCards(23, 25)}
-    if (y > 6700 && y < 7400){displayCards(25, 27)}
-    if (y > 7700 && y < 8400){displayCards(27, 29)}
-    if (y > 8100 && y < 8800){displayCards(29, 31)}
-    if (y > 8500 && y < 9200){displayCards(31, 33)}
-    if (y > 8900 && y < 9600){displayCards(33, 34)}
+    if (y > 4800 && y < 5400){displayCards(17, 19)}
+    if (y > 5200 && y < 5800){displayCards(19, 21)}
+    if (y > 5600 && y < 6200){displayCards(21, 23)}
+    if (y > 6500 && y < 7100){displayCards(23, 25)}
+    if (y > 6900 && y < 7500){displayCards(25, 27)}
+    if (y > 7300 && y < 7900){displayCards(27, 29)}
   })
 }
